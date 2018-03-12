510de60e9da5a68b3150b9a709991e7bc581a82f
leapm <luming.wang@hexagon.com>
2018-03-12 13:47:46 +0800

    add redux-form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import {combineReducers} from "redux"
 import user from './users';
+import { reducer as formReducer } from 'redux-form';
 
-export default combineReducers({user});
+export default combineReducers({user,form:formReducer});
